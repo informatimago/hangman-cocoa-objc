c8c78fa762a9d64e03c69741716be4d4c921b665
Pascal J. Bourguignon <pjb@informatimago.com>
2014-04-03 02:12:31 +0200

    Initial Commit

---
+++ hangman/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hangman
+//
+//  Created by Pascal Bourguignon on 2014-04-03.
+//  Copyright (c) 2014 informatimago. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ hangman/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  hangman
+//
+//  Created by Pascal Bourguignon on 2014-04-03.
+//  Copyright (c) 2014 informatimago. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
